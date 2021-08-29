811172f6bafd63b2494060667c44072d3d3f327e
Anastasy <snorrky@gmail.com>
2021-08-29 23:17:46 +0300

    мобильная версия и анимация, но анимацию надо доделать

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,15 @@ $(document).ready(function() {
         center: true,
         loop: true,
         nav: true,
-        navText: ["<img src='../images/icons/arrow-left.svg'>","<img src='../images/icons/arrow-right.svg'>"]
+        navText: ["<img src='../images/icons/arrow-left.svg'>","<img src='../images/icons/arrow-right.svg'>"],
+        responsive: {
+            0: {
+                nav: false
+            },
+            768: {
+                nav: true
+            }
+        }
     });
     
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
@@ -14,12 +22,31 @@ $(document).ready(function() {
         .closest('section.catalog').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    $('.catalog-item__link').each(function(i) {
+    toggleItem('.catalog-item__link');
+    toggleItem('.catalog-item__back');
+
+    $('[data-modal=consult]').on("click", function() {
+        $('.overlay, #consult').fadeIn();
+    });
+    $('.modal__close').on("click", function() {
+        $('.overlay, #consult, #buy, #thanks').fadeOut('slow');
+    });
+
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#buy .modal__desc').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #buy').fadeIn();
+        })
+    });
+}); 
+
+function toggleItem(link) {
+    $(link).each(function(i) {
         $(this).on("click", function(e) {
             e.preventDefault();
             $('.catalog-item__content').eq(i).toggleClass("catalog-item__content_active");
             $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
         })
-    })
-    
-}); 
+    });
+}
